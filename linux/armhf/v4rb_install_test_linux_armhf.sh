452e229f3f40242f9582a210668689916fa081b1
pashkovs <143988733+pashkovs@users.noreply.github.com>
2024-08-12 12:42:59 +0000

    Added armhf job

---
+++ linux/armhf/v4rb_install_test_linux_armhf.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+# Check if the VERSION parameter is provided
+if [ -z "$1" ]; then
+    echo "Please provide the VERSION to download as the first parameter."
+    exit 1
+fi
+
+# Assign the VERSION parameter to a variable
+VERSION="$1"
+# Extract the major VERSION from the provided VERSION
+MAJOR_VERSION=$(echo "$VERSION" | cut -d '.' -f 1)
+
+# Construct the DEB file name
+DEB_FILE="v4rb_armhf_32_${MAJOR_VERSION}_lin.deb"
+
+# Download the specified VERSION of the V4RB for macOS
+curl "https://valentina-db.com/download/prev_releases/$VERSION/lin_arm_32/$DEB_FILE" -o $DEB_FILE
+
+# Install the V4RB package
+sudo apt install ./$DEB_FILE
+
+V4RB_INSTALL_DIR="/opt/V4RB"
+
+# ValentinaPlugin.xojo_plugin is just ZIP, so we need to unzip it to a subfolder
+sudo unzip "$V4RB_INSTALL_DIR/ValentinaPlugin.xojo_plugin" -d "$V4RB_INSTALL_DIR/ValentinaPlugin"
+
+# Copy the V4RB dylib to the test project
+cp "$V4RB_INSTALL_DIR/ValentinaPlugin/ValentinaPlugin.xojo_plugin/Valentina/Build Resources/Linux ARM/v4rb_armhf_32_release.so" 'linux/armhf/TestProjectConsole/TestProjectConsole Libs'
+
+# Run the V4RB application and capture the output
+OUTPUT=$(linux/armhf/TestProjectConsole/TestProjectConsole)
+
+# Extract the Valentina Version from the output using awk
+VAL_VERSION=$(echo "$OUTPUT" | awk -F ': ' '/Valentina Version:/{print $2}' | xargs)
+
+echo "Valentina Version: $VAL_VERSION"
+echo "Expected Version: $VERSION"
+
+# Compare the extracted version with the passed parameter
+if [ "$VAL_VERSION" != "$VERSION" ]; then
+    echo "Error: Valentina Version ($VAL_VERSION) does not match the specified version ($VERSION)."
+    exit 1
+fi
