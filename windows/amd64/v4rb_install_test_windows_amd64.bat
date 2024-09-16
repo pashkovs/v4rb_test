9b3ff55197dd1a1832f9fc7bc51bca30cf9c86c5
pashkovs <143988733+pashkovs@users.noreply.github.com>
2024-09-16 10:08:07 +0000

    add logging

--- windows/amd64/v4rb_install_test_windows_amd64.bat
+++ windows/amd64/v4rb_install_test_windows_amd64.bat
@@ -21,7 +21,10 @@ REM Download the specified VERSION of the V4RB for Windows
 powershell -Command "Invoke-WebRequest -Uri https://valentina-db.com/download/prev_releases/%VERSION%/win_32/%EXE_FILE% -OutFile %EXE_PATH%"
 
 REM Install the V4RB package silently
-powershell -Command "Start-Process '%EXE_PATH%' -ArgumentList '/SILENT', '/CURRENTUSER', '/SUPPRESSMSGBOXES', '/NORESTART' -NoNewWindow -Wait"
+%EXE_PATH% /SILENT /NORESTART /CURRENTUSER /SUPPRESSMSGBOXES /LOG="%TEMP%/v4rb_log.txt"
+
+REM Print the contents of the log file
+type "%TEMP%/v4rb_log.txt"
 
 REM Define the Valentina plugin default installation directory
 set V4RB_INSTALL_DIR=%USERPROFILE%\Documents\Paradigma Software\V4RB_%MAJOR_VERSION%
