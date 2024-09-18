8c50208411d4e093ab1e445233ac2b9dad8e41a7
Serhii Pashkov <serpashk@gmail.com>
2024-09-19 00:40:08 +0300

    use self-hosted runner for arm64

---
+++ linux/arm64/vserver_install_test_linux_arm64.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+
+# Check if the VERSION parameter is provided
+if [ -z "$1" ]; then
+    echo "Please provide the VERSION to download as the first parameter."
+    exit 1
+fi
+
+# Assign the VERSION parameter to a variable
+VERSION="$1"
+# Extract the major VERSION from the provided VERSION
+MAJOR_VERSION=$(echo "$VERSION" | cut -d '.' -f 1)
+
+# Construct the DEB file name
+DEB_FILE="vserver_arm64_${MAJOR_VERSION}_lin.deb"
+
+# Download the specified VERSION of the V4RB for macOS
+curl "https://valentina-db.com/download/prev_releases/$VERSION/lin_arm_64/$DEB_FILE" -o $DEB_FILE
+
+# Install the package
+sudo apt install ./$DEB_FILE
+
+VSERVER_LOGS_DIR="/opt/VServer/vlogs"
+
+sleep 30
+
+ls -l $VSERVER_LOGS_DIR
+
+# The server startup takes some time, so need to try again after delay
+
+attempt=0
+max_attempts=5
+log_found=false
+
+
+while [ $attempt -lt $max_attempts ]; do
+    # Get the latest log file from the VServer logs directory with name starting with "vserver_" and ending with ".log"
+    VSERVER_LOG_FILE=$(ls -t "$VSERVER_LOGS_DIR"/vserver_*.log 2>/dev/null | head -n 1)
+
+    if [ -n "$VSERVER_LOG_FILE" ] && sudo grep -q "Server started" "$VSERVER_LOG_FILE"; then
+        log_found=true
+        break
+    fi
+
+    echo "Log file: $VSERVER_LOG_FILE"
+    sudo cat "$VSERVER_LOG_FILE"
+    echo "Log file not found or 'Server started' message not present. Retrying in 5 seconds..."
+    sleep 5
+    attempt=$((attempt + 1))
+done
+
+if [ "$log_found" = false ]; then
+    echo "Error: Server did not start successfully after $max_attempts attempts."
+    exit 1
+fi
+
+# Extract the Valentina Version from the log file using awk
+VAL_VERSION=$(sudo awk -F ': ' '/vServer version/{print $2}' "$VSERVER_LOG_FILE" | xargs)
+
+echo "Valentina Version: $VAL_VERSION"
+echo "Expected Version: $VERSION"
+
+# Compare the extracted version with the passed parameter
+if [ "$VAL_VERSION" != "$VERSION" ]; then
+    echo "Error: Valentina Version ($VAL_VERSION) does not match the specified version ($VERSION)."
+    exit 1
+fi
